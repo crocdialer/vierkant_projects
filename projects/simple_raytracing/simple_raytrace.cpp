c432cbf90332f2b536450d9b1e8a85ebe3cbe681
crocdialer <crocdialer@gmail.com>
2020-11-14 14:21:54 +0100

    adding a raytracing sample, based on VK_NV_ray_tracing

---
+++ projects/simple_raytracing/simple_raytrace.cpp
@@ -0,0 +1,171 @@
+#include <vierkant/imgui/imgui_util.h>
+
+#include "simple_raytrace.hpp"
+
+void SimpleRayTracing::setup()
+{
+    crocore::g_logger.set_severity(crocore::Severity::DEBUG);
+
+    create_context_and_window();
+    load_model();
+    create_graphics_pipeline();
+}
+
+void SimpleRayTracing::teardown()
+{
+    LOG_INFO << "ciao " << name();
+    vkDeviceWaitIdle(m_device->handle());
+}
+
+void SimpleRayTracing::poll_events()
+{
+    glfwPollEvents();
+}
+
+void SimpleRayTracing::create_context_and_window()
+{
+    m_instance = vk::Instance(g_enable_validation_layers, vk::Window::get_required_extensions());
+    m_window = vk::Window::create(m_instance.handle(), WIDTH, HEIGHT, name(), m_fullscreen);
+
+    // create device
+    vk::Device::create_info_t device_info = {};
+    device_info.physical_device = m_instance.physical_devices().front();
+    device_info.use_validation = m_instance.use_validation_layers();
+    device_info.surface = m_window->surface();
+
+    // add the raytracing-extension
+    device_info.extensions = {VK_NV_RAY_TRACING_EXTENSION_NAME};
+
+    m_device = vk::Device::create(device_info);
+
+    // Query the ray tracing properties of the current implementation, we will need them later on
+    VkPhysicalDeviceRayTracingPropertiesNV rayTracingProperties = {};
+    rayTracingProperties.sType = VK_STRUCTURE_TYPE_PHYSICAL_DEVICE_RAY_TRACING_PROPERTIES_NV;
+    VkPhysicalDeviceProperties2 deviceProps2{};
+    deviceProps2.sType = VK_STRUCTURE_TYPE_PHYSICAL_DEVICE_PROPERTIES_2;
+    deviceProps2.pNext = &rayTracingProperties;
+    vkGetPhysicalDeviceProperties2(m_device->physical_device(), &deviceProps2);
+
+//    // Query the ray tracing properties of the current implementation, we will need them later on
+////    VkPhysicalDeviceRay
+//    rayTracingFeatures.sType = VK_STRUCTURE_TYPE_PHYSICAL_DEVICE_RAY_TRACING_FEATURES_KHR;
+//    VkPhysicalDeviceFeatures2 deviceFeatures2{};
+//    deviceFeatures2.sType = VK_STRUCTURE_TYPE_PHYSICAL_DEVICE_FEATURES_2;
+//    deviceFeatures2.pNext = &rayTracingFeatures;
+//    vkGetPhysicalDeviceFeatures2(VkPhysicalDeviceRay, &deviceFeatures2);
+
+    m_window->create_swapchain(m_device, m_use_msaa ? m_device->max_usable_samples() : VK_SAMPLE_COUNT_1_BIT, V_SYNC);
+
+    // create a WindowDelegate
+    vierkant::window_delegate_t window_delegate = {};
+    window_delegate.draw_fn = std::bind(&SimpleRayTracing::draw, this, std::placeholders::_1);
+    window_delegate.resize_fn = [this](uint32_t w, uint32_t h)
+    {
+        create_graphics_pipeline();
+        m_camera->set_aspect(m_window->aspect_ratio());
+    };
+    window_delegate.close_fn = [this](){ set_running(false); };
+    m_window->window_delegates[name()] = window_delegate;
+
+    // create a KeyDelegate
+    vierkant::key_delegate_t key_delegate = {};
+    key_delegate.key_press = [this](const vierkant::KeyEvent &e)
+    {
+        if(!(m_gui_context.capture_flags() & vk::gui::Context::WantCaptureKeyboard))
+        {
+            if(e.code() == vk::Key::_ESCAPE){ set_running(false); }
+        }
+    };
+    m_window->key_delegates["main"] = key_delegate;
+
+    // create a gui and add a draw-delegate
+    m_gui_context = vk::gui::Context(m_device);
+    m_gui_context.delegates["application"] = [this]
+    {
+        vk::gui::draw_application_ui(std::static_pointer_cast<Application>(shared_from_this()), m_window);
+    };
+
+    // attach gui input-delegates to window
+    m_window->key_delegates["gui"] = m_gui_context.key_delegate();
+    m_window->mouse_delegates["gui"] = m_gui_context.mouse_delegate();
+
+    // camera
+    m_camera = vk::PerspectiveCamera::create(m_window->aspect_ratio(), 45.f, .1f, 100.f);
+    m_camera->set_position(glm::vec3(0.f, 0.f, 2.f));
+}
+
+void SimpleRayTracing::create_graphics_pipeline()
+{
+    const auto &framebuffers = m_window->swapchain().framebuffers();
+    auto fb_extent = framebuffers.front().extent();
+
+    vierkant::Renderer::create_info_t create_info = {};
+    create_info.num_frames_in_flight = framebuffers.size();
+    create_info.sample_count = m_window->swapchain().sample_count();
+    create_info.viewport = {0.f, 0.f, static_cast<float>(fb_extent.width),
+                            static_cast<float>(fb_extent.height), 0.f,
+                            static_cast<float>(fb_extent.depth)};
+
+    m_renderer = vierkant::Renderer(m_device, create_info);
+    m_gui_renderer = vierkant::Renderer(m_device, create_info);
+}
+
+void SimpleRayTracing::load_model()
+{
+    auto geom = vk::Geometry::create();
+    geom->vertices = {glm::vec3(-0.5f, -0.5f, 0.f),
+                      glm::vec3(0.5f, -0.5f, 0.f),
+                      glm::vec3(0.f, 0.5f, 0.f)};
+    geom->colors = {glm::vec4(1.f, 0.f, 0.f, 1.f),
+                    glm::vec4(0.f, 1.f, 0.f, 1.f),
+                    glm::vec4(0.f, 0.f, 1.f, 1.f)};
+    m_mesh = vk::Mesh::create_from_geometry(m_device, geom);
+
+    m_drawable = vk::Renderer::create_drawables(m_mesh).front();
+    m_drawable.pipeline_format.shader_stages = vierkant::create_shader_stages(m_device,
+                                                                              vierkant::ShaderType::UNLIT_COLOR);
+}
+
+void SimpleRayTracing::update(double time_delta)
+{
+    m_drawable.matrices.modelview = m_camera->view_matrix();
+    m_drawable.matrices.projection = m_camera->projection_matrix();
+
+    // issue top-level draw-command
+    m_window->draw();
+}
+
+std::vector<VkCommandBuffer> SimpleRayTracing::draw(const vierkant::WindowPtr &w)
+{
+    auto image_index = w->swapchain().image_index();
+    const auto &framebuffer = m_window->swapchain().framebuffers()[image_index];
+
+    auto render_mesh = [this, &framebuffer]() -> VkCommandBuffer
+    {
+        m_renderer.stage_drawable(m_drawable);
+        return m_renderer.render(framebuffer);
+    };
+
+    auto render_gui = [this, &framebuffer]() -> VkCommandBuffer
+    {
+        m_gui_context.draw_gui(m_gui_renderer);
+        return m_gui_renderer.render(framebuffer);
+    };
+
+    bool concurrant_draw = true;
+
+    if(concurrant_draw)
+    {
+        // submit and wait for all command-creation tasks to complete
+        std::vector<std::future<VkCommandBuffer>> cmd_futures;
+        cmd_futures.push_back(background_queue().post(render_mesh));
+        cmd_futures.push_back(background_queue().post(render_gui));
+        crocore::wait_all(cmd_futures);
+
+        // get values from completed futures
+        std::vector<VkCommandBuffer> command_buffers;
+        for(auto &f : cmd_futures){ command_buffers.push_back(f.get()); }
+        return command_buffers;
+    }
+    return {render_mesh(), render_gui()};
+}
---
+++ projects/simple_raytracing/simple_raytrace.hpp
@@ -0,0 +1,80 @@
+//
+// Created by crocdialer on 9/1/18.
+//
+
+#pragma once
+
+#include <crocore/Application.hpp>
+#include <crocore/Animation.hpp>
+
+#include <vierkant/vierkant.hpp>
+
+const int WIDTH = 1920;
+const int HEIGHT = 1080;
+const bool V_SYNC = true;
+bool DEMO_GUI = true;
+
+////////////////////////////// VALIDATION LAYER ///////////////////////////////////////////////////
+
+#ifdef NDEBUG
+const bool g_enable_validation_layers = false;
+#else
+const bool g_enable_validation_layers = true;
+#endif
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+
+class SimpleRayTracing : public crocore::Application
+{
+
+public:
+
+    explicit SimpleRayTracing(int argc = 0, char *argv[] = nullptr) : crocore::Application(argc, argv) {};
+
+private:
+
+    void setup() override;
+
+    void update(double time_delta) override;
+
+    void teardown() override;
+
+    void poll_events() override;
+
+    std::vector<VkCommandBuffer> draw(const vierkant::WindowPtr &w);
+
+    void create_context_and_window();
+
+    void create_graphics_pipeline();
+
+    void load_model();
+
+    bool m_use_msaa = true;
+
+    bool m_fullscreen = false;
+
+    // bundles basic Vulkan assets
+    vierkant::Instance m_instance;
+
+    // device
+    vierkant::DevicePtr m_device;
+
+    // window handle
+    std::shared_ptr<vierkant::Window> m_window;
+
+    vk::PerspectiveCameraPtr m_camera;
+
+    vk::MeshPtr m_mesh = vk::Mesh::create();
+
+    vk::Renderer::drawable_t m_drawable;
+
+    vk::Renderer m_renderer, m_gui_renderer;
+
+    vierkant::gui::Context m_gui_context;
+};
+
+int main(int argc, char *argv[])
+{
+    auto app = std::make_shared<SimpleRayTracing>(argc, argv);
+    return app->run();
+}
