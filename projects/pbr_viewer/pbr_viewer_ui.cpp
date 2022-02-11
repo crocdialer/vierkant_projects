1d0594b95c8e94f2267ca63dd2c538d11c649abe
crocdialer <crocdialer@gmail.com>
2022-02-11 17:05:59 +0100

    minor

--- projects/pbr_viewer/pbr_viewer_ui.cpp
+++ projects/pbr_viewer/pbr_viewer_ui.cpp
@@ -7,6 +7,7 @@
 #include <vierkant/imgui/imgui_util.h>
 
 #include "spdlog/sinks/base_sink.h"
+#include "spdlog/sinks/stdout_color_sinks.h"
 
 #include "pbr_viewer.hpp"
 
@@ -193,7 +194,7 @@ void PBRViewer::create_ui()
         ImVec2 window_pos = ImVec2((corner & 1) ? io.DisplaySize.x - DISTANCE : DISTANCE,
                                    (corner & 2) ? io.DisplaySize.y - DISTANCE : DISTANCE);
         ImVec2 window_pos_pivot = ImVec2((corner & 1) ? 1.0f : 0.0f, (corner & 2) ? 1.0f : 0.0f);
-        ImGui::SetNextWindowSizeConstraints(ImVec2(io.DisplaySize.x - 2 * DISTANCE, 200),
+        ImGui::SetNextWindowSizeConstraints(ImVec2(io.DisplaySize.x - 2 * DISTANCE, 220),
                                             ImVec2(io.DisplaySize.x - 2 * DISTANCE,
                                                    io.DisplaySize.y / 0.33f - 2 * DISTANCE));
         ImGui::SetNextWindowPos(window_pos, ImGuiCond_Always, window_pos_pivot);
@@ -202,14 +203,12 @@ void PBRViewer::create_ui()
         bool show_logger = true;
 
         ImGui::Begin("logger", &show_logger, ImGuiWindowFlags_NoMove | ImGuiWindowFlags_NoTitleBar |
-                                             ImGuiWindowFlags_NoResize |
                                              ImGuiWindowFlags_NoSavedSettings | ImGuiWindowFlags_NoFocusOnAppearing |
                                              ImGuiWindowFlags_NoNav);
         for(const auto &msg : m_log_queue)
         {
             ImGui::BulletText(msg.c_str());
-            ImGui::SetScrollHereY();
-//            ImGui::SetItemDefaultFocus();
+            if(ImGui::GetScrollY() >= ImGui::GetScrollMaxY()){ ImGui::SetScrollHereY(1.0f); }
         }
 
         ImGui::End();
