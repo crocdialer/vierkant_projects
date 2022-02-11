d44d0063aa0c3dbb25406f47ae432c5acac343a8
crocdialer <crocdialer@gmail.com>
2022-02-11 15:12:43 +0100

    submodule juggle

--- projects/pbr_viewer/pbr_viewer_ui.cpp
+++ projects/pbr_viewer/pbr_viewer_ui.cpp
@@ -51,20 +51,6 @@ void PBRViewer::create_ui()
     };
     imgui_sink->log_delegates["imgui"] = imgui_log;
 
-//    spdlog::info("Welcome to spdlog!");
-//    spdlog::error("Some error message with arg: {}", 1);
-//
-//    spdlog::warn("Easy padding in numbers like {:08d}", 12);
-//    spdlog::critical("Support for int: {0:d};  hex: {0:x};  oct: {0:o}; bin: {0:b}", 42);
-//    spdlog::info("Support for floats {:03.2f}", 1.23456);
-//    spdlog::info("Positional args are {1} {0}..", "too", "supported");
-//    spdlog::info("{:<30}", "left aligned");
-//
-//    spdlog::set_level(spdlog::level::debug); // Set global log level to debug
-//    spdlog::debug("This message should be displayed..");
-
-//    spdlog::get
-
     // create a KeyDelegate
     vierkant::key_delegate_t key_delegate = {};
     key_delegate.key_press = [this](const vierkant::KeyEvent &e)
