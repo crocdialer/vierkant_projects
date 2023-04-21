7c870a071a788a82d06a5009b345f4478601afab
crocdialer <crocdialer@gmail.com>
2023-04-21 23:01:41 +0200

    introduces ambient occlusion. ssao+rta, exposing only ssao here

--- projects/pbr_viewer/serialization.hpp
+++ projects/pbr_viewer/serialization.hpp
@@ -167,6 +167,7 @@ void serialize(Archive &archive, vierkant::PBRDeferred::settings_t &render_setti
             cereal::make_nvp("use_fxaa", render_settings.use_fxaa),
             cereal::make_nvp("environment_factor", render_settings.environment_factor),
             cereal::make_nvp("tonemap", render_settings.tonemap),
+            cereal::make_nvp("ambient_occlusion", render_settings.ambient_occlusion),
             cereal::make_nvp("bloom", render_settings.bloom),
             cereal::make_nvp("gamma", render_settings.gamma),
             cereal::make_nvp("exposure", render_settings.exposure),
