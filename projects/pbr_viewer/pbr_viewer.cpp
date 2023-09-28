68e26ba852c99e0ba79474247db22db57f3979ea
crocdialer <crocdialer@gmail.com>
2023-09-28 14:47:06 +0200

    do not request descriptor-buffer extension for now. Renderdoc can't handle it which was expected, question is will it ever

--- projects/pbr_viewer/pbr_viewer.cpp
+++ projects/pbr_viewer/pbr_viewer.cpp
@@ -192,7 +192,7 @@ void PBRViewer::create_context_and_window()
     }
 
     // upcoming usage of descriptor-buffer extension
-    device_info.extensions.push_back(VK_EXT_DESCRIPTOR_BUFFER_EXTENSION_NAME);
+//    device_info.extensions.push_back(VK_EXT_DESCRIPTOR_BUFFER_EXTENSION_NAME);
 
     m_device = vierkant::Device::create(device_info);
     m_window->create_swapchain(m_device, std::min(m_device->max_usable_samples(), m_settings.window_info.sample_count),
