1475b1a95e660e82c1b7743480ad007050b96f62
crocdialer <crocdialer@gmail.com>
2026-07-25 12:23:53 +0200

    serialize suppress_refractive_caustics

--- vierkant_cereal/include/vierkant_cereal/serialization.hpp
+++ vierkant_cereal/include/vierkant_cereal/serialization.hpp
@@ -361,7 +361,8 @@ void serialize(Archive &archive, vierkant::PBRPathTracer::settings_t &render_set
             cereal::make_optional_nvp("suppress_reset", render_settings.suppress_reset),
             cereal::make_optional_nvp("camera_medium", render_settings.camera_medium),
             cereal::make_optional_nvp("sunlight_params", render_settings.sunlight_params),
-            cereal::make_optional_nvp("mis_mode", render_settings.mis_mode));
+            cereal::make_optional_nvp("mis_mode", render_settings.mis_mode),
+            cereal::make_optional_nvp("suppress_refractive_caustics", render_settings.suppress_refractive_caustics));
 }
 
 template<class Archive>
